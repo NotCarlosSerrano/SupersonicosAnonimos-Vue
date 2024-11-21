8d49b1a163f7dfee70c3ddb2654c1fc2d2896942
Carlos Serrano <caseov@gmail.com>
2024-11-21 21:43:44 +0100

    Set default theme based on browser preference for dark mode

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -13,10 +13,14 @@ import { createVuetify } from 'vuetify'
 
 import { aliases, fa,  } from 'vuetify/iconsets/fa'
 
+// Get browser preference for dark mode
+const darkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+const defaultTheme = darkMode ? 'dark' : 'light'
+
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'light',
+    defaultTheme: defaultTheme,
   },
   icons: {
     defaultSet: 'mdi',
