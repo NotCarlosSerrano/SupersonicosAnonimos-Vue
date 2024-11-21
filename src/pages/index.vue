fd002b61bbe46bcbceff53e9e8774406c700b0fb
Carlos Serrano <caseov@gmail.com>
2024-11-21 21:44:06 +0100

    Refactor season data structure to use objects for better clarity and consistency

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -14,11 +14,24 @@ const colors = reactive({
   spotify: '#1DB954',
 })
 
-const selectedSeason = ref("Todas");
+const selectedSeason = ref({
+  title: 'Todas',
+  value: 'all',
+});
+
 const seasons = ref([
-  'Temporada 1',
-  'Temporada 2',
-  'Todas',
+  {
+    title: 'Temporada 1',
+    value: 'season1',
+  },
+  {
+    title: 'Temporada 2',
+    value: 'season2',
+  },
+  {
+    title: 'Todas',
+    value: 'all',
+  }
 ])
 
 const youtubeChannelUrl = "https://www.youtube.com/@SupersonicosAnonimos";
@@ -33,21 +46,21 @@ const links = ref([
     spotifyEpisodeId: "4QV3C42b2XIq10bul74mUt",
     title: "Capítulo 1: El del avión secreto del Pentágono",
     duration: "1:02:49",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "Jvfh1x_swMU",
     spotifyEpisodeId: "1WvXsr1mXcLaRyfhHmp7Z0",
     title: "Capítulo 2: El del accidente de avión en Tokio",
     duration: "1:00:00",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "nQXIoMZYL6I",
     spotifyEpisodeId: "5El1qkDUMKYZ8XWhdtgtTS",
     title: "Capítulo 3: El de la semana loca aeroespacial",
     duration: "1:02:16",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "0T00ZwqYROo",
@@ -56,21 +69,21 @@ const links = ref([
     subtitle: "Jorge Nicolás-Álvarez",
     subtitleUrl: "https://x.com/fugastronautica",
     duration: "58:56",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "ryHUipXzWgw",
     spotifyEpisodeId: "0JWlbjo8A6f2VdU074N5kP",
     title: "Capitulo 5: El del adiós a Ingenuity y el hola a SLIM",
     duration: "1:06:26",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "tEOq5ZmWyIU",
     spotifyEpisodeId: "0VFjD4fPj5LzhNaBFrtfbj",
     title: "Capitulo 6: Se calienta la cosa en Starbase",
     duration: "1:20:29",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "RFrEBZcAx8c",
@@ -79,7 +92,7 @@ const links = ref([
     subtitle: "Doctor Fisión",
     subtitleUrl: "https://x.com/doctorfision",
     duration: "57:30",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "csrMZAsCEq8",
@@ -88,154 +101,154 @@ const links = ref([
     subtitle: "Daniel Marín",
     subtitleUrl: "https://x.com/Eurekablog",
     duration: "1:24:34",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "yQbFgZGb-dA",
     spotifyEpisodeId: "38b2uBuIezH3zBaPE0BkOo",
     title: "Capitulo 9: Starship, vuelos supersónicos y la vuelta a la Luna",
     duration: "1:02:19",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "bi3wnk2Y2lY",
     spotifyEpisodeId: "4XTWNY8nGG9PhvU1bYoixJ",
     title: "Capitulo 10: Te subirías en el Primer Starship Tripulado?",
     duration: "1:07:15",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "BBdS1Lw5594",
     spotifyEpisodeId: "6biXe5l5oajB7r6DUuXHIl",
     title: "Capitulo 11: Especial Lanzamiento de Starship IFT-3",
     duration: "1:12:36",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "fD_shwAgbNs",
     spotifyEpisodeId: "00NDesaKCqvij14AY0ccZa",
     title: "Capitulo 12: ¿Éxito o fracaso de Starship?",
     duration: "1:15:24",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "Gv7lAuLBteQ",
     spotifyEpisodeId: "73WmAI6Vv8or5RUAxYNmDQ",
     title: "Capítulo 13: Cómo escapar de un cohete Soyuz en llamas",
     duration: "56:25",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "y7Mql3KrzDA",
     spotifyEpisodeId: "3FggKAGkuhMA0Olrbjy5Pj",
     title: "Capítulo 14: La Pólvora Negra y la búsqueda de la inmortalidad",
     duration: "1:33:32",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "W3E_llroUIY",
     spotifyEpisodeId: "30XlPDvkZvyaz7RUcXbzDQ",
     title: "Capítulo 15: Desmontando Terraplanistas",
     duration: "1:19:47",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "UAuShXH3FIA",
     title: "Capítulo 16: El Problema de la Voyager 1 y los Misiles de Irán",
     spotifyEpisodeId: "3eSqLNbEdFYfjwoi8snUdW",
     duration: "1:00:13",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "oJUjim6CkG4",
     spotifyEpisodeId: "3cRj6O8libzCtD9VAORNPQ",
     title: "Capítulo 17: Josep sobrevive a un B-737 Max SIN SABERLO",
     duration: "1:16:26",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "p_NfDTViF6c",
     spotifyEpisodeId: "0xFPfJwipJts9YvS0qtG7W",
     title: "Capítulo 18: SpaceX presenta sus nuevos trajes y BOEING lo vuelve a hacer",
     duration: "1:12:37",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "ulvrg1aVn-Y",
     spotifyEpisodeId: "2WIwMfdowjBIudeFoxH8Cy",
     title: "Capítulo 19: Las nuevas mejoras de Starship y las Auroras Boreales",
     duration: "1:07:43",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "IVGhTgKPGmo",
     spotifyEpisodeId: "3npdmYf4RhGImJ8ntYfqyT",
     title: "Consejos para Estudiantes y Carreras de Ciencias (y no tan ciencias)",
     duration: "2:00:38",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "e5vksFewaAQ",
     spotifyEpisodeId: "3hcIkhexVesJJd3cFZQ7fJ",
     title: "Capítulo 21: TODO lo que tienes que saber para el 4º Vuelo de Starship",
     duration: "54:32",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "swh61M_Ibxg",
     spotifyEpisodeId: "4L7ViINVlsEo532Twctbsh",
     title: "Capítulo 22: La locura de Starship y las nuevas informaciones de Elon Musk",
     duration: "1:07:06",
-    season: "Temporada 1",
+    season: "season1",
   },
   { 
     youtubeVideoId: "9QfMmn4aDnI", 
     spotifyEpisodeId: null,
     title: "Capítulo 23 / El definitivo",
     duration: "1:05:11",
-    season: "Temporada 1",
+    season: "season1",
   },
   {
     youtubeVideoId: "kGns4HEGlms", 
     spotifyEpisodeId: "1H8Ie7HExaSb7PIStCZ3Jj",
     title: "Capítulo 24 | HEMOS VUELTO",
     duration: "1:13:46",
-    season: "Temporada 2",
+    season: "season2",
   },
   {
     youtubeVideoId: "3QjDWMQehxg", 
     spotifyEpisodeId: "0KAmuZqkppMCtF2mAMqymm",
     title: "Capítulo 25 | La casi catástrofe aérea del avión de Josep",
     duration: "1:01:39",
-    season: "Temporada 2",
+    season: "season2",
   },
   {
     youtubeVideoId: "noggJKf9Mcc", 
     spotifyEpisodeId: "54wDQsNuD750OuSBo5tih4",
     title: "Capítulo 26 | PLD nos cuenta en EXCLUSIVA sus proyectos!",
     duration: "1:10:00",
-    season: "Temporada 2",
+    season: "season2",
   },
   {
     youtubeVideoId: "Ls-Om2iJtuc", 
     spotifyEpisodeId: "3HuXj8LwDdS87ObGFOOdAM",
     title: "Capítulo 27 | Histórico, Spacex atrapa un cohete gigantesco en el aire",
     duration: "1:05:10",
-    season: "Temporada 2",
+    season: "season2",
   },
   {
     youtubeVideoId: "ydmEKfvZD0I", 
     spotifyEpisodeId: "5naUvSnQYqNDM3mYsJvnNe",
     title: "Capítulo 28 | La NASA cerca de encontrar vida Extraterrestre",
     duration: "1:10:22",
-    season: "Temporada 2",
+    season: "season2",
   },
   {
     youtubeVideoId: "FPOSHUULFHY", 
     spotifyEpisodeId: "1WT6YyylKT2xW2iIDTOrRp",
     title: "Capítulo 29 | SpaceX secuestró focas para experimentar con ellas (Confirmado)",
     duration: "1:13:06",
-    season: "Temporada 2",
+    season: "season2",
   },
   {
     youtubeVideoId: "CzxeHx6CX8I", 
@@ -243,7 +256,7 @@ const links = ref([
     xVideoUrl: "https://x.com/Supersonicos_A/status/1853896253438128268",
     title: "Capítulo 30 | El Avión aniquilador Soviético y la Starship Lunar",
     duration: "1:21:25",
-    season: "Temporada 2",
+    season: "season2",
   },
   {
     youtubeVideoId: "jDV6tKZgx0g", 
@@ -251,7 +264,7 @@ const links = ref([
     xVideoUrl: "https://x.com/Supersonicos_A/status/1856419635928723809",
     title: "Capítulo 31 | Los planes de Elon Trump para ganar a China + Otro Lanzamiento Starship",
     duration: "1:20:05",
-    season: "Temporada 2",
+    season: "season2",
   },
   {
     youtubeVideoId: "09DgU-zmKc4", 
@@ -259,7 +272,7 @@ const links = ref([
     xVideoUrl: "https://x.com/Supersonicos_A/status/1857911419066310824",
     title: "Capítulo 32 | Elon Musk acabará con el SLS + Noticias de Starship",
     duration: "1:23:59",
-    season: "Temporada 2",
+    season: "season2",
   },
 ]);
 
@@ -293,12 +306,16 @@ function changeOrder() {
 
 const getItemsBySeason = computed(() => {
   return orderedLinks.value.filter((link) => {
-    if (selectedSeason.value === "Todas") {
+    if (selectedSeason.value.value === "all") {
       return true;
     }
-    return link.season === selectedSeason.value;
+    return link.season === selectedSeason.value.value;
   });
 });
+
+const getSeasonTitle = (season: string) => {
+  return seasons.value.find((s) => s.value === season)?.title;
+};
 
 </script>
 
@@ -309,10 +326,14 @@ const getItemsBySeason = computed(() => {
       :items="getItemsBySeason" 
       :items-per-page="itemsPerPage"
       :search="search"
-      class="border rounded-lg"
+      class="border"
+      style="border-top-left-radius: 9px;border-top-right-radius: 9px;"
+      :filter-keys="['title', 'subtitle']"
     >
       <template #header>
-        <v-toolbar class="px-2">
+        <v-toolbar
+          class="px-2 rounded-t-lg"
+        >
           <v-select
             v-model="selectedSeason"
             label="Temporadas"
@@ -323,6 +344,7 @@ const getItemsBySeason = computed(() => {
             variant="solo"
             class="mr-2"
             menu-icon="mdi-menu-down"
+            return-object
           />
           <v-text-field
             v-if="!xs"
@@ -427,11 +449,12 @@ const getItemsBySeason = computed(() => {
                     #default
                   >
                     <v-chip
-                      v-if="selectedSeason == 'Todas'"
+                      v-if="selectedSeason.value === 'all'"
                       class="ma-2"
                       color="secondary"
                       variant="flat"
-                      :text="item.raw.season"
+                      :text="getSeasonTitle(item.raw.season)"
+                      style="user-select: none;"
                     />
                   </template>
                 </v-img>
